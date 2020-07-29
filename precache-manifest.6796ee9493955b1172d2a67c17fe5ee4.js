871177529931f259efd4765a7fda1f0cfc1fa141
Aleksandr Mykhailov <ss.mykhailov@gmail.com>
2020-07-29 17:55:16 +0300

    Updates

---
+++ precache-manifest.6796ee9493955b1172d2a67c17fe5ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83991b95a680e6b6f0b46c09c25d4944",
+    "url": "/react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "371ecff82a09705f01ff",
+    "url": "/react-hw-02-feedback/static/css/main.5dca0d6f.chunk.css"
+  },
+  {
+    "revision": "5833f177daf46aec9570",
+    "url": "/react-hw-02-feedback/static/js/2.08172a33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-feedback/static/js/2.08172a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371ecff82a09705f01ff",
+    "url": "/react-hw-02-feedback/static/js/main.fcffa377.chunk.js"
+  },
+  {
+    "revision": "56ca9963ffb29c0e09b6",
+    "url": "/react-hw-02-feedback/static/js/runtime-main.e5c5a5e2.js"
+  }
+]);
